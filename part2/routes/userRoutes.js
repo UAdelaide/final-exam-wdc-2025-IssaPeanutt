866e3eccb14f700d74ced1a841e4029fa9f0a343
IssaPeanutt <diaqiaw@gmail.com>
2025-06-20 07:41:20 +0000

    [automatic commit] 2025-06-20 07:41:20 UTC 119.15.75.2

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -36,7 +36,7 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-router.get('/api/dogs', async (req, res) => {
+router.get('/dogs', async (req, res) => {
       try {
         const [rows] = await db.execute(`
           SELECT d.name AS dog_name, d.size, u.username AS owner_username
