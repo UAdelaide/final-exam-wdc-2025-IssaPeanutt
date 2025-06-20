38319fb817789c8c921f1a8e2e5a3438a99d8b60
IssaPeanutt <diaqiaw@gmail.com>
2025-06-20 07:13:29 +0000

    [automatic commit] 2025-06-20 07:13:29 UTC 119.15.75.2

--- part1/app.js
+++ part1/app.js
@@ -37,7 +37,7 @@ let db;
       password: '',
       database: 'DogWalkService'
     });
-app.get('/api/dogs', async (req, res) => {
+    app.get('/api/dogs', async (req, res) => {
       try {
         const [rows] = await db.execute(`
           SELECT d.name AS dog_name, d.size, u.username AS owner_username
